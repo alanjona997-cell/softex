669afc5709ba908b26c766887081a0fbfa5d785d
unknown <zaionkauanlw@gmail.com  git config --global user.name Zaion>
2025-08-21 16:53:23 -0300

    atv1.js

--- atv1/atv1.js
+++ atv1/atv1.js
@@ -1,13 +1,26 @@
 let readline = require(`readline-sync`)
 
-let nome = readline.question ("qual o seu nome?")
-console.log(`ola ${nome}`)
+let nome = readline.question ("ola, qual o seu nome?")
 
 let anoNascimento = readline.question ("qual a sua data de nascimento?")
-console.log(`você nasceu em ${anoNascimento}`)
 
-let pergunta = readline.question ("você mora sozinho?")
-console.log(`você: ${pergunta}`)
+let endereco = readline.question ("Onde você mora?")
 
-let pergunta2 = readline.question ("você tem animal?")
-console.log(`você ${pergunta2} animal`)
+let pergunta = readline.question ("qual a sua porofissao?")
+
+let pergunta1 = readline.question ("atualmente voce trabalha na sua area?")
+
+let pergunta2 = readline.question ("voce tem animal?")
+
+let pergunta3 = readline.question ("Qual o seu plano para daqui a 10 anos?")
+
+
+
+console.log(`-------------Formulário de ${nome}----------------`)
+console.log(`Voce nasceu em ${anoNascimento}`)
+console.log(`Voce mora em ${endereco}`)
+console.log(`Você é ${pergunta}`)
+console.log(`Atualmente você ${pergunta1} trabalha na area`)
+console.log(`Sobre animais, você ${pergunta2}`)
+console.log(`Seu plano para daqui a 10 anos é ${pergunta3}`)
+
